1ba747c242acd1e4652177b4f10eef2715d19a98
Agustín Ventura <agustinventura@gmail.com>
2019-01-29 18:16:41 +0100

    initial commit with screens and styles

---
+++ js/fifteenMinWorkout.js
@@ -0,0 +1,9 @@
+
+function init() {
+	setBackKeyListener();
+	//tizen.humanactivitymonitor.start('HRM', hrmListener);
+	//tizen.power.request("SCREEN", "SCREEN_NORMAL");
+	//showWelcomeScreen();
+}
+
+$(document).ready(init);
---
+++ js/framework.js
@@ -0,0 +1,67 @@
+function hideAllScreens() {
+	$("#welcomeScreen").hide();
+	$("#exerciseScreen").hide();
+	$("#setScreen").hide();
+	$("#endScreen").hide();
+}
+
+function setClickListener(element, listener) {
+	element.off("click");
+	element.click(listener);
+}
+
+function setRotaryListener(listener) {
+	$(document).off('rotarydetent');
+	$(document).on('rotarydetent', listener);
+}
+
+function setBackKeyListener() {
+	$(window).off('tizenhwkey');
+    $(window).on('tizenhwkey', function(e) {
+    	backPressed(e);
+    });
+}
+
+function backPressed(e) {
+    var activeDivId = $('.clock:visible').not(":hidden").prop("id")
+    if (e.originalEvent.keyName === 'back') {
+		goBack(activeDivId);
+    }
+}
+
+function goBack(activeDivId) {
+	switch (activeDivId) {
+		case "welcomeScreen":
+			exit();
+			break;
+		case "exerciseScreen":
+			showPauseScreen(activeDivId);
+			break;
+		case "countdownScreen":
+			showPauseScreen(activeDivId);
+			break;
+		case "endScreen":
+			exit();
+			break;
+	}
+}
+
+function exit() {
+	tizen.power.release("SCREEN");
+	tizen.humanactivitymonitor.stop('HRM');
+    tizen.application.getCurrentApplication().exit();
+}
+
+function hrmListener(hrmInfo) {
+	var currentHeartRate = preprendZerosIfNeeded(hrmInfo.heartRate, 3);
+	$("#exerciseBPM").text(currentHeartRate);
+}
+
+function preprendZerosIfNeeded(number, size) {
+	var numberLength = number.toString().length;
+	var numberOfZeroes = size - numberLength;
+	for (var i=0; i<numberOfZeroes; i++) {
+		number = "0" + number;
+	}
+	return number;
+}
